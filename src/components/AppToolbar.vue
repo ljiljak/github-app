8f14650729eb323a69802e929a930993a7fad518
ljiljak <ljiljakaleksandar@gmail.com>
2018-05-29 21:06:21 +0200

    Z2

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-ons-page>
+		 <v-ons-toolbar>
+		 	<div class="left">
+		 		<slote name="left"></slote>
+		 	</div>
+
+		 	<div class="center"> {{title}} </div>
+		 	
+		 	<div class="right">
+		 		<slote name="right"></slote>
+		 	</div>
+		 </v-ons-toolbar>
+	</v-ons-page>
+</template>
+
+<script>
+	export default {
+		props: {
+			title: {
+				type: $string,
+				default: 'Github App'
+			}
+		}
+
+	}
+
+</script>

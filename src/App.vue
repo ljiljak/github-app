b4008127bbcb67253d1fda0c7077019b443a8033
ljiljak <ljiljakaleksandar@gmail.com>
2018-06-01 21:17:37 +0200

    Z8

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         placeholder="Search">
      </AppSearch>
 
-      <v-ons-list v-if="!isLoading">
+      <v-ons-list v-if="!isLoading && repos.length">
       <v-ons-list-header>Repositories of {{query}}</v-ons-list-header>
       <v-ons-list-item v-for="repo in repos">
         <div class="left">
@@ -17,20 +17,13 @@
           <span class="list-item__title">{{repo.name}}</span><span class="list-item__subtitle">{{repo.description}}</span>
         </div>
       </v-ons-list-item>
-    </v-ons-list>
-
-    
-      
+    </v-ons-list>      
       <p v-if="isLoading">
         <v-ons-progress-circular indeterminate></v-ons-progress-circular>
       </p>
-
-
- 
-
-
-
-
+      <div v-if="!repos.length">
+        <EmptyState type="repository" />
+      </div>
   </div>
 
     </v-ons-page>
@@ -41,6 +34,7 @@ import AppToolbar from './components/AppToolbar.vue'
 import AppSearch from './components/AppSearch.vue'
 import { gitHub } from './services/GitHub.js'
 import debounce from 'lodash/debounce'
+import EmptyState from './components/EmptyState.vue'
 
 
 
@@ -48,14 +42,16 @@ import debounce from 'lodash/debounce'
 
     components: {
     AppToolbar,
-    AppSearch
+    AppSearch,
+    EmptyState
     },
 
     data() {
       return {
         query: '',
         repos: [],
-        isLoading: false
+        isLoading: false,
+      
       };
     },
 
@@ -67,6 +63,7 @@ import debounce from 'lodash/debounce'
           if (this.repos = !null) {
             this.repos = response.data
             this.isLoading = false
+        
           }
           //console.log(response)
         }).catch(()=> {
---
+++ src/components/EmptyState.vue
@@ -0,0 +1,29 @@
+<template>
+  
+ <v-ons-card>
+      <img src="https://pbs.twimg.com/media/Bt5MoW0CIAE9Fda.jpg" style="width: 100%">
+      
+      <div class="content">
+        There are no {{plural}} here
+      </div>
+    </v-ons-card>
+
+</template>
+
+<script>
+import pluralize from 'pluralize'
+
+ export default {
+    props: {
+        type: String,
+      },
+
+    computed: {
+      plural() {
+        return pluralize(this.type)
+      }
+    }
+
+  }
+
+</script> 
